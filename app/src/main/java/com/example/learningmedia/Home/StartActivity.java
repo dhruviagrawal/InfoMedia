d32dc55e4a89328227f2905df1bd02aca72e151d
anushka2422 <anushka2422saxena@gmail.com>
2022-07-01 04:24:02 +0530

    Upload Activity

--- app/src/main/java/com/example/learningmedia/Home/StartActivity.java
+++ app/src/main/java/com/example/learningmedia/Home/StartActivity.java
@@ -1,22 +1,15 @@
 package com.example.learningmedia.Home;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.app.FragmentTransaction;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.learningmedia.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class StartActivity extends AppCompatActivity {
@@ -49,16 +42,18 @@ public class StartActivity extends AppCompatActivity {
                         selectFragment[0] = new HomeFragment();
                         break;
                     case R.id.ic_profile:
+                       // selectFragment[0] = new ProfileFragment();
+                        SharedPreferences.Editor editor = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE).edit();
+                        editor.putString("profilefield", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                        editor.apply();
                         selectFragment[0] = new ProfileFragment();
                         break;
                     case R.id.ic_user:
                         selectFragment[0] = new UserFragment();
                         break;
                     case R.id.ic_upload:
-                        SharedPreferences.Editor editor = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE).edit();
-                        editor.putString("profilefield", FirebaseAuth.getInstance().getCurrentUser().getUid());
-                        editor.apply();
-                        selectFragment[0] = new UploadFragment();
+                        selectFragment[0]=null;
+                        startActivity(new Intent(StartActivity.this, UploadActivity.class));
                         break;
                 }
                 if (selectFragment[0] != null) {
